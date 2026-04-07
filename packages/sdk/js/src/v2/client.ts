b7acc286b956fe3797aa106b48f92784ec89e56d
Brendan Allan <git@brendonovich.dev>
2026-04-07 16:47:55 +0800

    sdk: throw error if response has text/html content type

--- packages/sdk/js/src/v2/client.ts
+++ packages/sdk/js/src/v2/client.ts
@@ -77,5 +77,11 @@ export function createOpencodeClient(config?: Config & { directory?: string; exp
       workspace: config?.experimental_workspaceID,
     }),
   )
+  client.interceptors.response.use((response) => {
+    const contentType = response.headers.get("content-type")
+    if (contentType === "text/html") throw new Error("Unexpected content-type: " + contentType)
+
+    return response
+  })
   return new OpencodeClient({ client })
 }
